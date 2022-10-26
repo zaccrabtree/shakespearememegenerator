a2056543f47b0cb38badf92a015bef15d3535ee3
Zac Crabtree <zac.crabtree1@gmail.com>
2022-10-25 19:25:36 -0500

    added text feature

--- shakespeare-meme-generator/src/Components/Meme.js
+++ shakespeare-meme-generator/src/Components/Meme.js
@@ -20,17 +20,41 @@ export default function Meme(){
         }))
         
     }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
     return (
         <>
             <div className="container">
                 <div className="row border border-dark">
                     <div className="col-6 float-start my-5 p-2 align-middle">
-                            <input type="text" className="m-2 p-2" placeholder="Top text"/>
-                            <input type="text" className="m-2 p-2" placeholder="Bottom text"/>
+                            <input 
+                                type="text" 
+                                className="m-2 p-2" 
+                                placeholder="Top text"
+                                name="topText"
+                                value={meme.topText}
+                                onChange={handleChange}
+                                />
+                            <input 
+                                type="text" 
+                                className="m-2 p-2" 
+                                placeholder="Bottom text"
+                                name="bottomText"
+                                value={meme.bottomText}
+                                onChange={handleChange}
+                                />
                             <button className="btn btn-success m-2 p-2" onClick={getMemeImage}>Get a New Meme Image</button>
                     </div>
-                    <div className="col-6 float-end my-5 p-2 ">
-                    <img src={meme.randomImage} className="rounded img-fluid"/>
+                    <div className="col-6 float-end my-5 p-2">
+                    <h2 className="carousel-caption my-5">{meme.topText}</h2>
+                    <img src={meme.randomImage} className="rounded img-fluid meme--image"/>
+                    <h2 className="carousel-caption">{meme.bottomText}</h2>
                     </div>
                 </div> 
                 <br/>
